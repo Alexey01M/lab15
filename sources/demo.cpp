ecb70895b9f54a27d91085eaf56b00c2de39ccba
Alexey01M <B2017-17VIU01M@yandex.ru>
2018-01-31 19:15:10 +0300

    123

--- sources/demo.cpp
+++ sources/demo.cpp
@@ -1,11 +1,12 @@
-#include <print.hpp>
+// "Copyright 2018 <AR>"
 #include <cstdlib>
+#include <print.hpp>
 
 int main(int argc, char** argv) {
         std::string text;
-	std::string log_path = std::getenv("LOG_PATH");
-	std::ofstream out(log_path, std::ios_base::app);
-        while(std::cin >> text) {
+        std::string log_path = std::getenv("LOG_PATH");
+        std::ofstream out(log_path, std::ios_base::app);
+        while (std::cin >> text) {
                 print(text, out);
                 out << std::endl;
         }
